a68e74b674e73b8352f668bb2ce053f2eb980ca6
Mohamed Babela <mbabela@e1r5p12.1337.ma>
2023-01-22 21:05:33 +0100

    db

--- transcendence_postgres_create.sql
+++ transcendence_postgres_create.sql
@@ -57,7 +57,7 @@ CREATE TABLE "public.friends" (
 	"ID" serial NOT NULL,
 	"Player1" TEXT NOT NULL UNIQUE,
 	"Player2" TEXT NOT NULL UNIQUE,
-	"date" DATE NOT NULL UNIQUE,
+	"date" DATE NOT NULL,
 	CONSTRAINT "friends_pk" PRIMARY KEY ("ID")
 ) WITH (
   OIDS=FALSE
@@ -90,6 +90,57 @@ CREATE TABLE "public.Titles" (
 
 
 
+CREATE TABLE "public.Channel" (
+	"ChannelId" serial NOT NULL,
+	"name" TEXT NOT NULL UNIQUE,
+	"Topic" TEXT,
+	"Key" TEXT,
+	"Memberlimits" int,
+	"Stats" TEXT NOT NULL DEFAULT 'public',
+	"Admin" TEXT NOT NULL,
+	CONSTRAINT "Channel_pk" PRIMARY KEY ("ChannelId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public.Block" (
+	"ID" serial NOT NULL,
+	"blocked" TEXT NOT NULL,
+	"date" DATE NOT NULL,
+	"reason" TEXT NOT NULL,
+	"nickname" TEXT NOT NULL,
+	CONSTRAINT "Block_pk" PRIMARY KEY ("ID")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public.Chat" (
+	"MsgId" serial NOT NULL,
+	"Player1" TEXT NOT NULL,
+	"Player2" TEXT NOT NULL,
+	"msg" TEXT NOT NULL,
+	CONSTRAINT "Chat_pk" PRIMARY KEY ("MsgId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public.Banned" (
+	"ChannelId" int NOT NULL,
+	"PlayerName" TEXT NOT NULL,
+	"reason" TEXT,
+	CONSTRAINT "Banned_pk" PRIMARY KEY ("ChannelId","PlayerName")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
 
 ALTER TABLE "Stats" ADD CONSTRAINT "Stats_fk0" FOREIGN KEY ("nickname") REFERENCES "Player"("nickname");
 ALTER TABLE "Stats" ADD CONSTRAINT "Stats_fk1" FOREIGN KEY ("Archiv") REFERENCES "Archievements"("Archiv");
@@ -102,7 +153,21 @@ ALTER TABLE "Match history" ADD CONSTRAINT "Match history_fk1" FOREIGN KEY ("pla
 
 ALTER TABLE "friends" ADD CONSTRAINT "friends_fk0" FOREIGN KEY ("Player1") REFERENCES "Player"("nickname");
 ALTER TABLE "friends" ADD CONSTRAINT "friends_fk1" FOREIGN KEY ("Player2") REFERENCES "Player"("nickname");
-ALTER TABLE "friends" ADD CONSTRAINT "friends_fk2" FOREIGN KEY ("date") REFERENCES "Player"("nickname");
+
+
+
+ALTER TABLE "Channel" ADD CONSTRAINT "Channel_fk0" FOREIGN KEY ("Admin") REFERENCES "Player"("nickname");
+
+ALTER TABLE "Block" ADD CONSTRAINT "Block_fk0" FOREIGN KEY ("blocked") REFERENCES "Player"("nickname");
+ALTER TABLE "Block" ADD CONSTRAINT "Block_fk1" FOREIGN KEY ("nickname") REFERENCES "Player"("nickname");
+
+ALTER TABLE "Chat" ADD CONSTRAINT "Chat_fk0" FOREIGN KEY ("Player1") REFERENCES "Player"("nickname");
+ALTER TABLE "Chat" ADD CONSTRAINT "Chat_fk1" FOREIGN KEY ("Player2") REFERENCES "Player"("nickname");
+
+ALTER TABLE "Banned" ADD CONSTRAINT "Banned_fk0" FOREIGN KEY ("ChannelId") REFERENCES "Channel"("ChannelId");
+ALTER TABLE "Banned" ADD CONSTRAINT "Banned_fk1" FOREIGN KEY ("PlayerName") REFERENCES "Player"("nickname");
+
+
 
 
 
