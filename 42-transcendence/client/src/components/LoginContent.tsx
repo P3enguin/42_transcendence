6c401b54a941c20af57ee1076cfa8bdb36b13df6
Pengu <sellamB66@gmail.com>
2023-01-23 18:35:13 +0100

    Add Home login elements and containers

---
+++ 42-transcendence/client/src/components/LoginContent.tsx
@@ -0,0 +1,25 @@
+function LoginContent() {
+    return (<div className = "home-login-content">
+        <div className = "login-game">
+            <p>here is the game simulation</p>
+        </div>
+        <div className = "login-description">
+            <div className = "login-description-container">
+                <p>Welcome to our website! 
+                    We are glad you are here and hope you enjoy
+                    playing Pong and chatting with other users.
+                    Our features include the ability to create or 
+                    join games, view available games and the leader-board,
+                    add friends, and view stats. 
+                    There is also a chat feature that allows you to send messages,
+                     create and join rooms, and view online users. Thank you for 
+                    choosing our website. 
+                    Have a great time!
+                    </p>
+            </div>
+            <button>continue With Intra</button>
+        </div>
+    </div>);
+}
+
+export default LoginContent;
--- 42-transcendence/client/src/pages/index.tsx
+++ 42-transcendence/client/src/pages/index.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import Layout from '@/components/Layout'
-
+import LoginContent from '@/components/LoginContent'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -16,9 +16,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <body>
-
-      </body>
+      <LoginContent/>
     </div>
     // <>
 
