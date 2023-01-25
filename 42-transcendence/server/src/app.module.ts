fbb04b84898ea0368566ecf3463316045cc350b3
Pengu <sellamB66@gmail.com>
2023-01-25 17:39:28 +0100

    Create auth controller

--- 42-transcendence/server/src/app.module.ts
+++ 42-transcendence/server/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsController } from './cats/cats.controller';
+import { AuthController } from './auth/auth.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, CatsController, AuthController],
   providers: [AppService],
 })
 export class AppModule {}
