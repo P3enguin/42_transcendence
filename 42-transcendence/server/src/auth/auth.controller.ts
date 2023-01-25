1d802c97722d872c8ed9a21a10bf19786cfcc2ba
Pengu <sellamB66@gmail.com>
2023-01-25 17:55:14 +0100

    Add Service and module to the auth

--- 42-transcendence/server/src/auth/auth.controller.ts
+++ 42-transcendence/server/src/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller,Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) {}
+
+    @Post('login')
+    login() {
+
+    }
+}
