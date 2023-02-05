799d7daa704f42d07a877c0d6db6343284fc9078
Pengu <sellamB66@gmail.com>
2023-02-05 14:20:53 +0100

    Remove server side authentication files

--- 42-transcendence/server/src/app.module.ts
+++ 42-transcendence/server/src/app.module.ts
@@ -2,14 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthController } from './auth/auth.controller';
-import { AuthModule } from './auth/auth.module';
 import { JwtModule } from '@nestjs/jwt';
 
 
 
 
 @Module({
-  imports: [ConfigModule.forRoot(),AuthModule],
+  imports: [ConfigModule.forRoot()],
 })
 export class AppModule {}
