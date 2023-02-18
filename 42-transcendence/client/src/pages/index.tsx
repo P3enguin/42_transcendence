bf6c8b9120738921aa7c189aaa084df47f8ccad0
Yasser Bensellam <ybensell@e2r7p6.1337.ma>
2023-02-18 14:38:17 +0100

    styling text and button

--- 42-transcendence/client/src/pages/index.tsx
+++ 42-transcendence/client/src/pages/index.tsx
@@ -4,6 +4,7 @@ import { Inter, Nabla } from '@next/font/google'
 import Layout from '@/components/Layout'
 import LoginContent from '@/components/login/LoginContent'
 import NavBarLayout from '@/components/Layout'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -17,6 +18,56 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      {/* <div className="flex items-center  flex-wrap justify-center sm:justify-start">
+      <Image 
+            src="/game.png"
+            alt='game'
+            width={800}
+            height={600}
+          />
+      <div className="ml-4 sm:ml-8 text-center sm:text-left">
+        <h2 className="text-2xl font-bold">Pognitor</h2>
+        <p className="text-gray-500 mt-2">Your text description goes here.</p>
+      </div>
+    </div> */}
+
+        <div className="relative pt-12 bg-blueGray-50">
+        <div className="items-center flex flex-wrap justify-center">
+          <div className=" px-4">
+          <Image 
+            src="/game.png"
+            alt='game'
+            width={500}
+            height={600}
+          />
+          </div>
+          <div className="mt-4 sm:ml-8 text-center sm:text-left ">
+              <h3 className="text-2xl font-bold text-white">Ponigator</h3>
+              <p className="mt-4 text-lg leading-relaxed text-white ">
+                  The Ultimate Exprerience For The Mighty Ping Context!
+              </p>
+              <div className='pt-5'>
+                <button className=" uppercase mx-auto shadow
+                bg-[#0097E2] hover:bg-indigo-700 focus:shadow-outline 
+                focus:outline-none text-white text-s font-bold py-2 px-12 rounded-full">JOIN NOW</button>
+              </div>
+
+          </div>
+        </div>
+        </div>
+
+        {/* <div classNameNameName="flex items-center justify-center h-screen flex-wrap">
+          <Image 
+            src="/game.png"
+            alt='game'
+            width={800}
+            height={600}
+          />
+          <div classNameNameName="">
+            <p>Ponigator</p>
+            <p>The Ultimate Exprerience For The Mighty Ping Context!</p>
+          </div>
+        </div> */}
     </>
   ) 
 }
