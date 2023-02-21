d5a0f3de51f7cbd56f12fca1f99c656959562b7f
pengu <sellamB66@gmail.com>
2023-02-21 14:36:02 +0100

    Responsive display for login forms

--- 42-transcendence/client/src/components/home/LoginHomeComp.tsx
+++ 42-transcendence/client/src/components/home/LoginHomeComp.tsx
@@ -9,10 +9,10 @@ function LoginComponent() {
     const [text,changeText]=useState([{text:"Sign In",status:true},
                                   {text:"gg",status:false}]);
 
-    return (<div className="w-1/4 flex flex-col text-2xl ">
+    return (<div className="w-2/3 xl:w-1/3 md:max-xl:w-1/2 flex flex-col text-base md:text-2xl">
         <motion.div key="login" initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}} 
             className="">
-            <div className="text-white flex flex-col items-center gap-3 ">
+            <div className="text-white flex flex-col items-center gap-5 ">
                 <p >CONTINUE WITH</p>
                 <div className="flex flex-row gap-5">
                     <motion.button whileHover={{scale: 1.5}} 
@@ -41,15 +41,14 @@ function LoginComponent() {
                 </div>
                 <p>OR</p>
             </div>
-            <div>
-            <form className="flex flex-col items-center ">
+            <form className="flex flex-col items-center">
                 <div className="relative z-0 w-3/4 mb-6 group">
                     <input type="input" name="username" id="username" 
                     className="block py-2.5 px-0 w-full text-sm text-white bg-transparent 
                     border-0 border-b-2 border-gray-300 appearance-none dark:text-white
                      dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none 
                      focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                    <label htmlFor="username" className="peer-focus:font-medium absolute text-sm
+                     <label htmlFor="username" className="peer-focus:font-medium absolute text-sm
                      text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 
                      scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600
                       peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 
@@ -69,16 +68,15 @@ function LoginComponent() {
                        peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
                 </div>
                     <motion.div key="buttonSign" layout={false} layoutId='button'  initial={false} transition={{type: "Tween" }}
-                        className="self-center w-3/4" >
+                        className="self-center  w-3/4" >
                         <button className="uppercase w-full  shadow bg-[#0097E2] hover:bg-[#2C3B7C] 
                                 transform transition duration-300 
-                                hover:text-l hover:scale-110  text-white text-s hover:text-md
-                                font-bold py-2 px-12  rounded-full">
+                                hover:text-l hover:scale-110  text-white text-xs  text-center md:text-base hover:text-md
+                                 py-2 px-12  rounded-full">
                                 SIGN IN
                         </button>
                     </motion.div>
             </form>
-            </div>
         </motion.div>
     </div>  );
 }
