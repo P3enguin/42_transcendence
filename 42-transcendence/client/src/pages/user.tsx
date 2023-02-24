acf66039f4dc1a43e09758d9548e70eb9eb26505
pengu <sellamB66@gmail.com>
2023-02-24 20:05:16 +0100

    I forgot what I added

--- 42-transcendence/client/src/pages/user.tsx
+++ 42-transcendence/client/src/pages/user.tsx
@@ -7,7 +7,7 @@ function UserHomePage() {
          from-[#28346C] via-[#121C46] to-[#263268]">
 
         </div>
-        <div className="  absolute top-10 z-10 flex flex-col items-center w-16 h-full overflow-hidden
+        <div className="  absolute top-10 z-10 flex flex-col items-center w-16 h-5/6 overflow-hidden
              text-gray-400 border rounded-tr-3xl rounded-bl-3xl 
               border-[#0097E2] bg-gradient-to-t from-[#141E4A] to-[#28346C] rounded">
 		<a className="flex items-center justify-center mt-3" href="#">
@@ -18,7 +18,7 @@ function UserHomePage() {
 
         </div>
 		<div className="flex flex-col items-center mt-3">
-			<a className="flex items-center justify-center w-12 h-12 mt-2 rounded hover:bg-gray-700 hover:text-gray-300" href="#">
+			<a className="flex items-center justify-center w-12 h-12 mt-2 rounded  " href="#">
 				<svg className="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 				 	<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
 				</svg>
