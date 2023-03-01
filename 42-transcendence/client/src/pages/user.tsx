f2f20959a95e0e8bc89db0d764c83d4c0f611d2a
pengu <sellamB66@gmail.com>
2023-03-01 12:57:52 +0100

    Element Item navbar responsive add

--- 42-transcendence/client/src/pages/user.tsx
+++ 42-transcendence/client/src/pages/user.tsx
@@ -1,22 +1,57 @@
 import Image from "next/image";
 import { HomeIcon, ChatBubbleOvalLeftEllipsisIcon } from "@heroicons/react/24/outline";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import { Disclosure } from "@headlessui/react";
 import { Menu } from '@headlessui/react'
-
+import { motion,AnimatePresence } from "framer-motion";
 
 function UserHomePage() {
 
     const [svgIndex,setSvgIndex] = useState(0);
+    const [isVisible,setVisible] = useState(false);
+
+    const variants = {
+        open: { opacity: 1},
+        closed: { opacity: 0 },
+    }
 
     function handleSvgIndex(index : number) {
         setSvgIndex(index)
     }
 
+    function toggleSideBar() {
+        setVisible(!isVisible);
+    }
+    useEffect(() => {
+        const handleResize = () => {
+          // Get the current screen width
+          const screenWidth = window.innerWidth;
+    
+          // If the screen width is medium or larger, hide the sidebar
+          if (screenWidth >= 640) {
+            setVisible(true);
+          }
+          else
+            setVisible(false);
+        };
+    
+        // Add a resize event listener to the window object
+        window.addEventListener('resize', handleResize);
+    
+        // Call the resize handler initially to ensure that the sidebar is hidden if necessary
+        handleResize();
+    
+        // Remove the resize event listener when the component is unmounted
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+      }, []);
+
     return (
 
     <div className="h-screen sm:p-10">
         {/* navbar */}
+
         <div className="  w-full h-16 z-0 bg-gradient-to-r border  border-[#0097E2] rounded-tr-3xl rounded-bl-3xl
         from-[#28346C] via-[#121C46] to-[#263268] flex flex-row items-center sm:justify-center justify-between">
             <div className="flex flex-row gap-10 items-center">
@@ -25,7 +60,8 @@ function UserHomePage() {
                 </a>
                 <button type="button" className="text-[#8BD9FF] rounded-lg sm:hidden hover:bg-[#01FD91] hover:bg-opacity-50
                     hover:text-[#01FD91] focus:outline-none focus:ring-2 focus:ring-gray-200 focus:text-[#01FD91] dark:text-gray-400 dark:hover:bg-gray-700
-                    dark:focus:ring-gray-600" aria-controls="navbar-search" aria-expanded="false">
+                    dark:focus:ring-gray-600" aria-controls="navbar-search" aria-expanded="false"
+                    onClick={toggleSideBar}>
                     <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
                 </button>
             </div>
@@ -51,9 +87,14 @@ function UserHomePage() {
         </div>
 
         {/* SideBar */}
-        <div className=" absolute top-0 sm:top-10 z-10  flex-col items-center w-16 h-5/6 overflow-hidden
+
+        <motion.nav  animate={isVisible ? "open" : "closed"}
+                variants={variants}
+                exit={{ opacity: 0 }}
+                >
+        <div id="userSideBar" className={`absolute top-0 sm:top-10 z-10  flex-col items-center w-16 h-5/6 overflow-hidden
             border rounded-tr-3xl rounded-bl-3xl border-[#0097E2] bg-gradient-to-t from-[#141E4A]
-            to-[#28346C] gap-6 sm:flex hidden">
+            to-[#28346C] gap-6    ${isVisible ? 'flex' : 'hidden'}` }>
             <a className="flex items-center justify-center mt-3" href="#" >
                 <Image src="/logo.png" alt="logo" width={39} height={41} />
             </a>
@@ -99,18 +140,33 @@ function UserHomePage() {
                     </svg>
                 </a>
             </div>
-            <div className="absolute bottom-0 mb-3 ">
-                <a className="flex items-center justify-center w-12 h-12 mt-2 " href="#">
-                    <svg width="29" height="30" viewBox="0 0 29 30" fill="none" xmlns="http://www.w3.org/2000/svg"
-                        className="fill-[#8BD9FF] hover:fill-[#0097E2] ease-in duration-200">
-                        <path d="M14.2681 19.245C11.8614 19.245 9.91455 17.37 9.91455 15.015C9.91455 12.66 11.8614 10.77 14.2681 10.77C16.6748 10.77 18.5756 12.66 18.5756 15.015C18.5756 17.37 16.6748 19.245 14.2681 19.245Z" />
-                        <path opacity="0.4" d="M28.0952 18.555C27.8039 18.105 27.39 17.655 26.8535 17.37C26.4243 17.16 26.1484 16.815 25.9031 16.41C25.1213 15.12 25.5812 13.425 26.8842 12.66C28.4171 11.805 28.9077 9.9 28.0185 8.415L26.9915 6.645C26.1177 5.16 24.2015 4.635 22.6839 5.505C21.3349 6.225 19.6027 5.745 18.8209 4.47C18.5757 4.05 18.4377 3.6 18.4684 3.15C18.5143 2.565 18.3304 2.01 18.0545 1.56C17.4873 0.63 16.4602 0 15.3258 0H13.1644C12.0454 0.03 11.0183 0.63 10.4511 1.56C10.1598 2.01 9.99122 2.565 10.0219 3.15C10.0525 3.6 9.91457 4.05 9.6693 4.47C8.88751 5.745 7.15529 6.225 5.82164 5.505C4.2887 4.635 2.38786 5.16 1.49876 6.645L0.471688 8.415C-0.402085 9.9 0.0884544 11.805 1.60606 12.66C2.90906 13.425 3.36894 15.12 2.60247 16.41C2.34187 16.815 2.06594 17.16 1.63672 17.37C1.11552 17.655 0.655641 18.105 0.410371 18.555C-0.156815 19.485 -0.126157 20.655 0.44103 21.63L1.49876 23.43C2.06594 24.39 3.12367 24.99 4.22738 24.99C4.74858 24.99 5.36175 24.84 5.85229 24.54C6.23553 24.285 6.69541 24.195 7.20128 24.195C8.71888 24.195 9.99122 25.44 10.0219 26.925C10.0219 28.65 11.4322 30 13.2104 30H15.2952C17.0581 30 18.4684 28.65 18.4684 26.925C18.5143 25.44 19.7867 24.195 21.3043 24.195C21.7948 24.195 22.2547 24.285 22.6533 24.54C23.1438 24.84 23.7417 24.99 24.2782 24.99C25.3666 24.99 26.4243 24.39 26.9915 23.43L28.0645 21.63C28.6164 20.625 28.6624 19.485 28.0952 18.555Z" />
+
+            <div className="flex flex-col absolute bottom-0 items-center gap-7">
+                <div className="flex sm:hidden">
+                    <svg width="27" height="27" viewBox="0 0 27 27" fill="none" xmlns="http://www.w3.org/2000/svg" 
+                            className= "fill-[#8BD9FF] hover:fill-red-700 ease-in duration-200">
+                        <path opacity="0.4" d="M0 5.92933C0 2.66133 2.70698 0 6.03271 0H12.6475C15.9665 0 18.6667 2.65333 18.6667 5.916V20.7373C18.6667 24.0067 15.9597 26.6667 12.6326 26.6667H6.0205C2.7002 26.6667 0 24.0133 0 20.7507V19.4973V5.92933Z" />
+                        <path d="M26.3719 12.6064L22.5775 8.72771C22.1853 8.32771 21.5542 8.32771 21.1634 8.73037C20.7738 9.13304 20.7751 9.78237 21.166 10.1824L23.245 12.3064H21.2517H10.0646C9.5127 12.3064 9.0647 12.7664 9.0647 13.333C9.0647 13.901 9.5127 14.3597 10.0646 14.3597H23.245L21.166 16.4837C20.7751 16.8837 20.7738 17.533 21.1634 17.9357C21.3594 18.137 21.6153 18.2384 21.8724 18.2384C22.1269 18.2384 22.3827 18.137 22.5775 17.9384L26.3719 14.061C26.5602 13.8677 26.6667 13.6064 26.6667 13.333C26.6667 13.061 26.5602 12.7997 26.3719 12.6064Z" />
                     </svg>
-                </a>
+                </div>
+                <div className="  mb-3 ">
+                    <a className="flex items-center justify-center w-12 h-12 mt-2 " href="#">
+                        <svg width="29" height="30" viewBox="0 0 29 30" fill="none" xmlns="http://www.w3.org/2000/svg"
+                            className="fill-[#8BD9FF] hover:fill-[#0097E2] ease-in duration-200">
+                            <path d="M14.2681 19.245C11.8614 19.245 9.91455 17.37 9.91455 15.015C9.91455 12.66 11.8614 10.77 14.2681 10.77C16.6748 10.77 18.5756 12.66 18.5756 15.015C18.5756 17.37 16.6748 19.245 14.2681 19.245Z" />
+                            <path opacity="0.4" d="M28.0952 18.555C27.8039 18.105 27.39 17.655 26.8535 17.37C26.4243 17.16 26.1484 16.815 25.9031 16.41C25.1213 15.12 25.5812 13.425 26.8842 12.66C28.4171 11.805 28.9077 9.9 28.0185 8.415L26.9915 6.645C26.1177 5.16 24.2015 4.635 22.6839 5.505C21.3349 6.225 19.6027 5.745 18.8209 4.47C18.5757 4.05 18.4377 3.6 18.4684 3.15C18.5143 2.565 18.3304 2.01 18.0545 1.56C17.4873 0.63 16.4602 0 15.3258 0H13.1644C12.0454 0.03 11.0183 0.63 10.4511 1.56C10.1598 2.01 9.99122 2.565 10.0219 3.15C10.0525 3.6 9.91457 4.05 9.6693 4.47C8.88751 5.745 7.15529 6.225 5.82164 5.505C4.2887 4.635 2.38786 5.16 1.49876 6.645L0.471688 8.415C-0.402085 9.9 0.0884544 11.805 1.60606 12.66C2.90906 13.425 3.36894 15.12 2.60247 16.41C2.34187 16.815 2.06594 17.16 1.63672 17.37C1.11552 17.655 0.655641 18.105 0.410371 18.555C-0.156815 19.485 -0.126157 20.655 0.44103 21.63L1.49876 23.43C2.06594 24.39 3.12367 24.99 4.22738 24.99C4.74858 24.99 5.36175 24.84 5.85229 24.54C6.23553 24.285 6.69541 24.195 7.20128 24.195C8.71888 24.195 9.99122 25.44 10.0219 26.925C10.0219 28.65 11.4322 30 13.2104 30H15.2952C17.0581 30 18.4684 28.65 18.4684 26.925C18.5143 25.44 19.7867 24.195 21.3043 24.195C21.7948 24.195 22.2547 24.285 22.6533 24.54C23.1438 24.84 23.7417 24.99 24.2782 24.99C25.3666 24.99 26.4243 24.39 26.9915 23.43L28.0645 21.63C28.6164 20.625 28.6624 19.485 28.0952 18.555Z" />
+                        </svg>
+                    </a>
+                </div>
             </div>
-        </div>       
+        </div> 
+
+        </motion.nav>
     </div>);
-    
+
 }
 
 export default UserHomePage;
+
+
+
