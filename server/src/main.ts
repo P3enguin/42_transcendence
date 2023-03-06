fd59a41b792e1c078a24b5efd68c9b8863bd5a89
4reha <ael-hadd@student.1337.ma>
2023-03-06 11:31:28 +0100

    fixing 'expected ,' error

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,7 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: {
-    origin: process.env.FRONTEND_HOST
+    origin: process.env.FRONTEND_HOST,
     credentials: true,
   }});
   app.use(cookieParser());
