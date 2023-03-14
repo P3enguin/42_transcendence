81448335ca9e90592a3c3018debc9d70f9a50f5e
naahio <mohamadbabela@gmail.com>
2023-03-14 04:12:14 +0000

    updating

--- server/src/prisma/prisma.service.ts
+++ server/src/prisma/prisma.service.ts
@@ -16,6 +16,7 @@ export class PrismaService extends PrismaClient {
 			},
 		});
 	}
+	
 
 	GetStatus(statusId: number): Promise<Status> {
 		const status = this.status.findUnique({
@@ -110,4 +111,7 @@ export class PrismaService extends PrismaClient {
 	}
 
 }
+
+
+
 
