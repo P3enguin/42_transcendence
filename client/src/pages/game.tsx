5b06ce0d9a242c3da20fbca4864701dc8203e9b7
4reha <ael-hadd@student.1337.ma>
2023-03-16 04:22:11 +0100

    Adding socket client

--- client/src/pages/game.tsx
+++ client/src/pages/game.tsx
@@ -1,14 +1,36 @@
-import Layout from "@/components/layout/layout";
-import { verifyToken } from "@/components/VerifyToken";
+import Layout from '@/components/layout/layout';
+import { verifyToken } from '@/components/VerifyToken';
+import Head from 'next/head';
+import { useEffect } from 'react';
+import { io, Socket } from 'socket.io-client';
+let socket: Socket;
 
 function Game({ jwt_token }: { jwt_token: string }) {
+  useEffect(() => {
+    socket = io('http://localhost:8000/game', {
+      auth: {
+        token: jwt_token,
+      },
+    });
+    socket.on('connect', () => {
+      console.log('connected');
+      socket.emit('message', { username: 'test', message: 'hello' });
+    });
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
   return (
-      <></>
+    <>
+      <Head>
+        <title>Ponginator | Start Game</title>
+      </Head>
+    </>
   );
 }
 
 export async function getServerSideProps({ req }: any) {
-  const jwt_token: string = req.cookies["jwt_token"];
+  const jwt_token: string = req.cookies['jwt_token'];
 
   if (jwt_token) {
     const res = await verifyToken(req.headers.cookie);
@@ -23,18 +45,14 @@ export async function getServerSideProps({ req }: any) {
   }
   return {
     redirect: {
-      destination: "/",
+      destination: '/',
       permanent: true,
     },
   };
 }
 
-Game.getLayout = function getLayout(page:React.ReactNode) {
-  return (
-    <Layout>
-      {page}
-    </Layout>
-  )
-}
+Game.getLayout = function getLayout(page: React.ReactNode) {
+  return <Layout>{page}</Layout>;
+};
 
 export default Game;
