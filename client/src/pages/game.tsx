40239f06b637129c0acfa80bc76155088c882526
4reha <ael-hadd@student.1337.ma>
2023-03-19 15:44:23 +0000

    removing checked by default on game type

--- client/src/pages/game.tsx
+++ client/src/pages/game.tsx
@@ -53,11 +53,11 @@ function Game({ jwt_token, data }: { jwt_token: string; data: [] }) {
           <div className="flex h-full flex-col rounded-2xl p-5 md:w-[50%]">
             <h2 className="m-2 text-lg font-bold md:text-2xl">START A GAME</h2>
             <div className="felx m-auto mb-0 flex-row self-center">
-              <input type="radio" name="type" checked />
+              <input type="radio" name="type"/>
               <label htmlFor="default" className=" m-1">
                 Normal Game
               </label>
-              <input type="radio" name="type" checked />
+              <input type="radio" name="type"/>
               <label htmlFor="default" className=" m-1">
                 Ranked Game
               </label>
