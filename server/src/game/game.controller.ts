58a800aca6a4198cb35f93b73aca60b8b9e200aa
4reha <ael-hadd@student.1337.ma>
2023-03-21 17:08:45 +0000

    adding api game route

--- server/src/game/game.controller.ts
+++ server/src/game/game.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  UseGuards,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { GameService } from './game.service';
+import { JwtGuard } from 'src/auth/guard';
 
 @Controller('game')
-export class GameController {}
+export class GameController {
+  constructor(private gameService: GameService) {}
+
+  @Get('join')
+  @UseGuards(JwtGuard)
+  joinGame(@Req() req: Request, @Res() res: Response) {
+    console.log(req.body.jwtDecoded);
+    
+    // return res.status(200).json(req.body.jwtDecoded);
+  }
+}
