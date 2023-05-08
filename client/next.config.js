1a492a8ac8bab7a749d323efec7d4eb1b004ea36
4reha <ael-hadd@student.1337.ma>
2023-05-08 15:29:20 +0100

    fixing next.config

--- client/next.config.js
+++ client/next.config.js
@@ -1,32 +1,34 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-        remotePatterns: [{
-            protocol: 'http',
-            hostname: 'localhost',
-            port: '8000',
-            pathname: '/avatars/**',
-        },
-        {
-            protocol: 'http',
-            hostname: 'localhost',
-            port: '8000',
-            pathname: '/wallpapers/**',
-        },
-        {
-            protocol: 'http',
-            hostname: 'localhost',
-            port: '8000',
-            pathname: '/ranks/**',
-        },
-        {
-            protocol: 'http',
-            hostname: 'localhost',
-            port: '8000',
-            pathname: '/rooms/**',
-        }, ],
-    },
-}
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '**',
+        port: '8000',
+        pathname: '/avatars/**',
+      },
+      {
+        protocol: 'http',
+        hostname: '**',
+        port: '8000',
+        pathname: '/wallpapers/**',
+      },
+      {
+        protocol: 'http',
+        hostname: '**',
+        port: '8000',
+        pathname: '/ranks/**',
+      },
+      {
+        protocol: 'http',
+        hostname: '**',
+        port: '8000',
+        pathname: '/rooms/**',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
