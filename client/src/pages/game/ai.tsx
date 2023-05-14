15db538f254ac4d8a92122de32a20e774a47bf34
4reha <ael-hadd@student.1337.ma>
2023-05-14 09:06:55 +0100

    Protect ai route

--- client/src/pages/game/ai.tsx
+++ client/src/pages/game/ai.tsx
@@ -3,21 +3,23 @@ import PongAi from '@/components/game/PongAi';
 import ScoreBoard from '@/components/game/ScoreBoard';
 import Layout from '@/components/layout/layout';
 import Head from 'next/head';
+import NextApiRequest from 'next';
 import { useRef, useState } from 'react';
+import { verifyToken } from '@/components/VerifyToken';
+import axios from 'axios';
 
 const Ai = () => {
   const gameRef = useRef<HTMLDivElement>(null);
   const [AIScore, setAIScore] = useState<number>(0);
   const [PlayerScore, setPlayerScore] = useState<number>(0);
 
-
-  const newScore = (player:string) => {
-    if(player === 'AI'){
-      setAIScore(AIScore + 1)
-    }else{
-      setPlayerScore(PlayerScore + 1)
+  const newScore = (player: string) => {
+    if (player === 'AI') {
+      setAIScore(AIScore + 1);
+    } else {
+      setPlayerScore(PlayerScore + 1);
     }
-  }
+  };
 
   return (
     <>
@@ -30,16 +32,43 @@ const Ai = () => {
       >
         {/* <div>Play with Ai</div> */}
         <ScoreBoard
-          P1="Player"
-          P1Score={PlayerScore}
-          P2="AI"
-          P2Score={AIScore}
+          player1="Player"
+          player1Score={PlayerScore}
+          player2="AI"
+          player2Score={AIScore}
         />
-        <PongAi gameRef={gameRef} newScore={newScore} isSimulation={false} />
+        <PongAi
+          gameRef={gameRef}
+          newScore={newScore}
+          isSimulation={false}
+          isContact={false}
+          isRotated={false}
+        />
       </div>
     </>
   );
 };
+
+export async function getServerSideProps({ req }: NextApiRequest) {
+  const jwt_token = req.cookies['jwt_token'];
+
+  if (jwt_token) {
+    const res = await verifyToken(req.headers.cookie);
+    if (res.ok) {
+      return {
+        props: {
+          jwt_token: jwt_token,
+        },
+      };
+    }
+  }
+  return {
+    redirect: {
+      destination: '/',
+      permanent: true,
+    },
+  };
+}
 
 Ai.getLayout = function getLayout(page: React.ReactNode) {
   return <Layout>{page}</Layout>;
