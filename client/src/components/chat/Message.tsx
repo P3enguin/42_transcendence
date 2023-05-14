1bd63b7f89636f67089cc50bb2e4ae7ef0f9f694
4reha <ael-hadd@student.1337.ma>
2023-05-14 09:07:26 +0100

    Fixing chat bubble

--- client/src/components/chat/Message.tsx
+++ client/src/components/chat/Message.tsx
@@ -5,13 +5,21 @@ function Message({message, side}: {message: any, side: string}) {
     // }, [message])
 
     return (
-        <div className="">
-            <div className="imessage conversation">
-                <p className={`${side} `}>{message.message} from {message.sender} {side}</p>
-                <p>{message.time}</p>
-            </div>
+        <>
+        <div className="imessage conversation">
+          <p className={`${side} `}>
+          <object data="/chat_tail_right.svg" type="image/svg+xml" className=" absolute right-0 translate-x-1 bottom-0" ></object>
+            {message.message} from {message.sender} {side}
+          </p>
+          <p>{message.time}</p>
+          <p className={`${side} `}>
+          <object data="/chat_tail_left.svg" type="image/svg+xml" className=" absolute left-0 -translate-x-1 bottom-0" ></object>
+            {message.message} from {message.sender} {side}
+          </p>
+          <p>{message.time}</p>
         </div>
-    )
+        </>
+    );
 }
 
 export default Message;
